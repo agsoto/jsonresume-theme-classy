6805e533e3c741b3b3a77d8b4a503072f4f1d07c
A. Soto <agsoto@protonmail.com>
2026-01-16 18:52:42 -0500

    chore: update preview assets and scripts

--- scripts/generate-preview.js
+++ scripts/generate-preview.js
@@ -1,27 +1,35 @@
 import path from 'node:path';
 import puppeteer from 'puppeteer';
-import { render } from '../src/index.js';
+import { render } from '../dist/index.js';
 import resume from '../test/fixture.resume.json' with { type: 'json' };
+import resumeLogo from '../test/fixture.resume-logo.json' with { type: 'json' };
 
 const html = await render(resume);
 
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
 await page.setViewport({
-  width: 779,
+  width: 900,
   height: 1054,
 });
 await page.setContent(html);
 await page.screenshot({
-  path: path.join('assets', 'preview-light.png'),
+  path: path.join('assets', 'preview.png'),
 });
-await page.emulateMediaFeatures([
-  {
-    name: 'prefers-color-scheme',
-    value: 'dark',
-  },
-]);
+
+const htmlLogo = await render(resumeLogo);
+await page.setContent(htmlLogo);
 await page.screenshot({
-  path: path.join('assets', 'preview-dark.png'),
+  path: path.join('assets', 'preview-logo.png'),
 });
+//TODO: Someday add dark mode and the preview, maybe after ten years are gone...
+// await page.emulateMediaFeatures([
+//   {
+//     name: 'prefers-color-scheme',
+//     value: 'dark',
+//   },
+// ]);
+// await page.screenshot({
+//   path: path.join('assets', 'preview-dark.png'),
+// });
 await browser.close();
